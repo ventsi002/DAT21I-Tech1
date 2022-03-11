0c6402ab56e21fd88c8da11f489ae4bd7366928d
ventsi002 <viliev2002@gmail.com>
2022-03-11 16:27:44 +0100

    ChinokDB and LibraryDB homework

--- Library_Homework/src/dml.sql
+++ Library_Homework/src/dml.sql
@@ -68,11 +68,78 @@ AND tbookcopy.nBookID = tbook.nBookID
 AND tloan.cCPR = tmember.cCPR;
 
 #13.9
-SELECT cTitle, CName
-FROM tbook
-INNER JOIN tbooktheme ON tbook.nBookID = tbook.nBookID
+SELECT tbook.cTitle, ttheme.cName FROM tbook
+INNER JOIN tbooktheme ON tbook.nBookID = tbooktheme.nBookID
 INNER JOIN ttheme ON ttheme.nThemeID = tbooktheme.nThemeID
-WHERE nPublishingCompanyID = '32';
+WHERE tbook.nPublishingCompanyID = 32;
+
+#13.10
+SELECT cName, cSurname, COUNT(cTitle) FROM tbook
+INNER JOIN tauthorship ON tbook.nBookID = tauthorship.nBookID
+INNER JOIN tauthor ON tauthor.nAuthorID = tauthorship.nAuthorID
+WHERE cTitle IS NOT NULL
+GROUP BY cName, cSurname;
+
+#13.11
+SELECT CONCAT(cName, ' ' ,cSurname) AS 'Author',
+       MIN(nPublishingYear) AS 'Lowest publishing year' FROM tauthor
+INNER JOIN tauthorship ON tauthorship.nAuthorID = tauthor.nAuthorID
+INNER JOIN tbook ON tbook.nBookID = tauthorship.nBookID
+GROUP BY Author;
+
+#13.12
+SELECT tbook.cTitle, tmember.cName, tmember.cSurname FROM tloan
+INNER JOIN tbookcopy ON tloan.cSignature = tbookcopy.cSignature
+INNER JOIN tmember ON tloan.cCPR = tmember.cCPR
+INNER JOIN tbook ON tbook.nBookID = tbookcopy.nBookID;
+
+
+#14
+SELECT cName, cTitle FROM ttheme, tbooktheme, tbook
+WHERE ttheme.nThemeID = tbooktheme.nThemeID
+AND tbooktheme.nBookID = tbook.nBookID;
+# I could not display the themes without books
+
+#15
+SELECT cName, cSurname, cTitle FROM tmember, tbook, tbookcopy, tloan
+WHERE tbook.nBookID = tbookcopy.nBookID
+AND tbookcopy.cSignature = tloan.cSignature
+AND tloan.cCPR = tmember.cCPR
+AND dNewMember BETWEEN '2013-01-01' AND '2013-12-31'
+AND dLoan BETWEEN '2013-01-01' AND '2013-12-31'
+ORDER BY cName, cSurname;
+
+#16
+SELECT tauthor.cName, tauthor.cSurname, tcountry.cName, if(tauthor.nAuthorID <> tauthorship.nBookID, tbook.cTitle, '') AS TITLE
+FROM tauthor
+INNER JOIN tnationality ON tauthor.nAuthorID = tnationality.nAuthorID
+INNER JOIN tcountry ON tnationality.nCountryID = tcountry.nCountryID
+INNER JOIN tauthorship ON tauthor.nAuthorID = tauthorship.nAuthorID
+INNER JOIN tbook ON tauthorship.nBookID = tbook.nBookID
+ORDER BY  tauthor.cName, cSurname;
+
+#17
+SELECT book1.cTitle AS 'Books with releases in 1970 and 1989' FROM tbook book1, tbook book2
+WHERE book1.cTitle = book2.cTitle
+AND book1.nPublishingYear = 1970
+AND book2.nPublishingYear = 1989;
+
+#18
+SELECT Names FROM(
+SELECT DISTINCT CONCAT(library.tmember.cSurname, ' ', library.tmember.cName) AS 'Names' FROM tmember
+WHERE dNewMember BETWEEN '2013-12-1' AND '2013-12-31'
+UNION ALL SELECT CONCAT(tauthor.cName, ' ', tauthor.cSurname) AS 'Authors named William' FROM tauthor
+WHERE cName = 'William') AS SUB;
+
+#19
+SELECT cName, cSurname FROM tmember ORDER BY dNewMember ASC LIMIT 1;
+
+#20
+# I could not understand the question.
+
+#21
+SELECT tpublishingcompany.cName AS Name, tcountry.cName AS Country FROM tpublishingcompany, tcountry
+WHERE tpublishingcompany.nCountryID = tcountry.nCountryID;
 
 #22
 SELECT cTitle FROM tbook
@@ -101,9 +168,12 @@ AND tbook.nBookID = tbooktheme.nBookID
 AND tbooktheme.nThemeID = ttheme.nThemeID;
 
 #27
-SELECT cTitle FROM tbook, tbookcopy, tloan
-WHERE tbook.nBookID = tbookcopy.nBookID
-AND tbookcopy.cSignature = tloan.cSignature
+SELECT cTitle FROM tbook
+WHERE nBookID NOT IN
+      (SELECT nBookID FROM tbookcopy
+        WHERE cSignature IN
+              (SELECT cSignature FROM tloan
+                WHERE tloan.cSignature IS NOT NULL));
 
 #28
 SELECT cName, COUNT(tbook.nPublishingCompanyID) AS 'No. of Books' FROM tbook, tpublishingcompany
@@ -115,8 +185,12 @@ SELECT COUNT(cCPR) AS 'Member who took a book in 2013' FROM tloan
 WHERE dLoan BETWEEN '2013-01-01' AND '2013-12-31';
 
 #30
-SELECT cTitle, COUNT(tauthor.nAuthorID) AS 'No. of Authors' FROM tbook, tauthor, tauthorship
-GROUP BY cTitle HAVING 'No. of Authors' >= 2
+SELECT tbook.cTitle, COUNT(tauthor.nAuthorID) AS 'No. of Authors'
+FROM tbook, tauthor, tauthorship
+WHERE tauthorship.nAuthorID = tauthor.nAuthorID
+AND tauthorship.nBookID = tbook.nBookID
+GROUP BY tbook.cTitle
+HAVING `No. of Authors` > 1
 
 
 
